1fd3aa5e3ac23bc1cf74b141aad16174c00a6bd1
JAP994 <sucop@MacBook-Pro-de-Jonathan.local>
2025-07-23 15:08:46 -0500

    Gitlab and Github vinculados

--- src/main/java/springboot_app/controllers/GreetingRestController.java
+++ src/main/java/springboot_app/controllers/GreetingRestController.java
@@ -9,6 +9,6 @@ public class GreetingRestController {
 
     @GetMapping("/saludo/{name}")
     public String greeting(@PathVariable String name){
-        return "Hola como te va" + name;
+        return "Gitlab and Github sincronized" + name;
     }
 }
