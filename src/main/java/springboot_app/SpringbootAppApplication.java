a58784e81c348484d5663a4200848174b654ff0c
JAP994 <sucop@MacBook-Pro-de-Jonathan.local>
2025-07-31 20:35:59 -0500

    Configuracion zona horaria en la aplicacion

--- src/main/java/springboot_app/SpringbootAppApplication.java
+++ src/main/java/springboot_app/SpringbootAppApplication.java
@@ -1,7 +1,11 @@
 package springboot_app;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SpringbootAppApplication {
@@ -9,5 +13,11 @@ public class SpringbootAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootAppApplication.class, args);
 	}
+
+	@PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Guayaquil"));
+        System.out.println("Zona horaria establecida a: " + TimeZone.getDefault());
+    }
 
 }
